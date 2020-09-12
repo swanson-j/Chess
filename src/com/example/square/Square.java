e5519752330074ffee8523dd3b119af73a4c7b3e
swanson-j <swansonj@mail.usf.edu>
2020-09-11 20:56:07 -0400

    Added member variables and methods

--- src/com/example/square/Square.java
+++ src/com/example/square/Square.java
@@ -1,5 +1,40 @@
 package com.example.square;
 
+import com.example.piece.Piece;
+
 public class Square {
+    private int x;
+    private int y;
+    private Piece piece;
+
+    public Square(int x, int y, Piece piece){
+        this.piece = piece;
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public Piece getPiece(){
+        return this.piece;
+    }
+
+    public void setPiece(Piece piece){
+        this.piece = piece;
+    }
 
 }
