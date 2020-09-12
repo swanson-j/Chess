eb491a9bb550d093af406bef8ae6c17da5700300
swanson-j <swansonj@mail.usf.edu>
2020-09-11 21:59:14 -0400

    Added pawn class

---
+++ src/com/example/piece/Pawn.java
@@ -0,0 +1,24 @@
+package com.example.piece;
+
+import com.example.board.Board;
+import com.example.square.Square;
+
+public class Pawn extends Piece {
+    private boolean hasMoved = false;
+
+    public Pawn(boolean isWhite, boolean isCaptured){
+        super(isWhite, isCaptured);
+    }
+
+    public boolean hasMoved(){
+        return this.hasMoved;
+    }
+
+    public void setHasMoved(boolean hasMoved){
+        this.hasMoved = hasMoved;
+    }
+
+    public boolean canMove(Board board, Square startSquare, Square endSquare){
+
+    }
+}
