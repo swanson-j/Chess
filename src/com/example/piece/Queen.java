3aeb216726ee9705772b9a0df6eb10052f4a8f3e
swanson-j <swansonj@mail.usf.edu>
2020-09-11 22:25:54 -0400

    Added queen class

--- src/com/example/piece/Queen.java
+++ src/com/example/piece/Queen.java
@@ -1,2 +1,16 @@
-package com.example.piece;public class Queen {
+package com.example.piece;
+
+import com.example.board.Board;
+import com.example.square.Square;
+
+public class Queen extends Piece{
+    public Queen(boolean isWhite, boolean isCaptured){
+        super(isWhite, isCaptured);
+    }
+
+    @Override
+    public boolean canMove(Board board, Square startSquare, Square endSquare) {
+        //TODO
+        return false;
+    }
 }
