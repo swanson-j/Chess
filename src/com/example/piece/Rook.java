8b38e885cad6ea0cdbe00c568ea000a9e351e53b
swanson-j <swansonj@mail.usf.edu>
2020-09-11 22:26:26 -0400

    Added rook class

--- src/com/example/piece/Rook.java
+++ src/com/example/piece/Rook.java
@@ -1,2 +1,16 @@
-package com.example.piece;public class Rook {
+package com.example.piece;
+
+import com.example.board.Board;
+import com.example.square.Square;
+
+public class Rook extends Piece{
+    public Rook(boolean isWhite, boolean isCaptured){
+        super(isWhite, isCaptured);
+    }
+
+    @Override
+    public boolean canMove(Board board, Square startSquare, Square endSquare) {
+        //TODO
+        return false;
+    }
 }
