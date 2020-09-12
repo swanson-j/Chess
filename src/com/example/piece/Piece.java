ce73265c34de60bb83f7622d7e449019e0645ef2
swanson-j <swansonj@mail.usf.edu>
2020-09-11 21:22:33 -0400

    Added member variables and methods

--- src/com/example/piece/Piece.java
+++ src/com/example/piece/Piece.java
@@ -1,6 +1,34 @@
 package com.example.piece;
 
-public abstract class Piece {
+import com.example.board.Board;
+import com.example.square.Square;
 
+public abstract class Piece {
+    private boolean isWhite;
+    private boolean isCaptured;
+
+    public Piece(boolean isWhite, boolean isCaptured){
+        setIsWhite(isWhite);
+        setIsCapture(isCaptured);
+    }
+
+    public boolean isWhite(){
+        return this.isWhite;
+    }
+
+    public void setIsWhite(boolean isWhite){
+        this.isWhite = isWhite;
+    }
+
+    public boolean isCaptured(){
+        return this.isCaptured;
+    }
+
+    public void setIsCapture(boolean isCaptured){
+        this.isCaptured = isCaptured;
+    }
+
+    // Check if piece is making a legal move
+    public abstract boolean canMove(Board board, Square startSquare, Square endSquare);
 }
 
