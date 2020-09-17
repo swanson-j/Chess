1ccfefc00c8116b149dc8b40fcb84bae282836ef
Joshua <swansonj@mail.usf.edu>
2020-09-17 02:53:39 -0400

    added bishop moves

--- src/com/example/piece/Bishop.java
+++ src/com/example/piece/Bishop.java
@@ -21,9 +21,43 @@ public class Bishop extends Piece {
         int yMove = endSquare.getY() - startSquare.getY();
 
         // bishop is not moving diagonally
-        if(Math.abs(xMove) != Math.abs(yMove)){
+        if(Math.abs(xMove) != Math.abs(yMove) || xMove == 0 || yMove == 0){
             return false;
         }
-        return false;
+
+        // check that no pieces are contained within the path of the bishop
+        if(xMove > 0){
+            if(yMove > 0){
+                for(int i = 1; i != xMove; i++){
+                    if(board.board[startSquare.getX()+i][startSquare.getY()+i].getPiece() != null){
+                        return false;
+                    }
+                }
+                return true;
+            }else{
+                for(int i = 1; i != xMove; i++){
+                    if(board.board[startSquare.getX()+i][startSquare.getY()-i].getPiece() != null){
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }else{
+            if(yMove > 0){
+                for(int i = -1; i != xMove; i--){
+                    if(board.board[startSquare.getX()+i][startSquare.getY()-i].getPiece() != null){
+                        return false;
+                    }
+                }
+                return true;
+            }else{
+                for(int i = -1; i != xMove; i--){
+                    if(board.board[startSquare.getX()+i][startSquare.getY()+i].getPiece() != null){
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
     }
 }
