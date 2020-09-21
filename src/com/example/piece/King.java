c59dda2b594940812219170c4e5335c20bce1061
Joshua <swansonj@mail.usf.edu>
2020-09-21 16:51:04 -0400

    added king move functionality

--- src/com/example/piece/King.java
+++ src/com/example/piece/King.java
@@ -4,12 +4,39 @@ import com.example.board.Board;
 import com.example.square.Square;
 
 public class King extends Piece{
+
+    private boolean hasCastled = false;
+
     public King(boolean isWhite, boolean isCaptured){
         super(isWhite, isCaptured);
     }
 
+    public boolean hasCastled(){
+        return this.hasCastled;
+    }
+
+    public void setHasCastled(boolean hasCastled){
+        this.hasCastled = hasCastled;
+    }
+
     @Override
     public boolean canMove(Board board, Square startSquare, Square endSquare) {
-        return false;
+        // rook cannot capture same colored piece
+        if(endSquare.getPiece() != null){
+            if(endSquare.getPiece().isWhite() == this.isWhite())
+                return false;
+        }
+
+        //TODO: Finish castling functionality
+
+        int xMove = endSquare.getX() - startSquare.getX();
+        int yMove = endSquare.getY() - startSquare.getY();
+
+        // King is not moving correctly
+        if((xMove == 0 && yMove == 0) || Math.abs(xMove) > 1 || Math.abs(yMove) > 1){
+            return false;
+        }
+
+        return true;
     }
 }
