ff1d9d981a150e5917e01b9e1d772b5580962d1b
Joshua <swansonj@mail.usf.edu>
2020-09-22 02:44:40 -0400

    added game class

--- src/com/example/game/Game.java
+++ src/com/example/game/Game.java
@@ -1,4 +1,28 @@
 package com.example.game;
 
+import com.example.board.Board;
+import com.example.move.Move;
+import com.example.player.Player;
+
+import java.util.List;
+
 public class Game {
+    private Board board;
+    private Player[] players;
+    private List<Move> moves;
+
+    public Game(){
+        board = new Board();
+        players = new Player[2];
+    }
+
+    public Board getBoard(){
+        return this.board;
+    }
+
+    public void startGame(Player player1, Player player2){
+        board.reset();
+        players[0] = player1;
+        players[1] = player2;
+    }
 }
