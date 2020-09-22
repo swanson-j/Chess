2fdfe8c8322c7619dff6981536013128f4aa867d
Joshua <swansonj@mail.usf.edu>
2020-09-22 02:45:25 -0400

    altered play to contain game object

--- src/com/example/play/Play.java
+++ src/com/example/play/Play.java
@@ -1,10 +1,19 @@
 package com.example.play;
 
 import com.example.board.Board;
+import com.example.game.Game;
+import com.example.player.Player;
 
 public class Play {
     public static void main(String[] args) {
-        Board board = new Board();
-        board.printBoard();
+        Game game = new Game();
+
+        Player player1 = new Player(true);
+        Player player2 = new Player(false);
+
+        game.startGame(player1, player2);
+        game.getBoard().printBoard();
+
+        System.out.println(game.getBoard().board[0][0].getPiece().canMove(game.getBoard(), game.getBoard().board[1][0], game.getBoard().board[4][0]));
     }
 }
